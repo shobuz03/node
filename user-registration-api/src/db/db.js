ab2aabb7870c8aa6296636ee2732a1c0e7aca3f8
Zayn An Nisa <shobuz_cse03@yahoo.com>
2019-12-29 21:32:57 +0600

    update from home

--- user-registration-api/src/db/db.js
+++ user-registration-api/src/db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/user-registration-db', {
+//mongoose.connect('mongodb://localhost/user-registration-db', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
--- user-registration-api/testdb.js
+++ user-registration-api/testdb.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test2');
+mongoose.connect('mongodb+srv://ahasanmongo:Ahasan03@cluster0-p58cc.mongodb.net/test?retryWrites=true&w=majority');
 //todo schema
-let todoSchema = new mongoose.Schema(
-    {
-        name: String,
-        completed: Boolean
-    }
-);
+let todoSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean
+});
 
 //todo model
 let Todo = mongoose.model('Todo', todoSchema);
 //save data into database
-Todo.create({ name: 'this is test data1', completed: true }).then((err, todo) => {
+Todo.create({
+    name: 'this is test data1',
+    completed: true
+}).then((err, todo) => {
     console.log(err, todo);
 });
