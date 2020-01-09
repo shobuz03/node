bd71e5e241cb22e4a8a8ecec512e1744d77a34c5
Ahasan Habib <shobuz.cse03@gmail.com>
2020-01-09 13:08:30 +0600

    fixed express file upload

--- express-fileupload/app.js
+++ express-fileupload/app.js
@@ -1,10 +1,36 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const fileUpload = require('express-fileupload')
-const port = 8000
-
+var bodyParser = require('body-parser')
+const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(fileUpload())
+app.use('/static', express.static('public'))
+app.get('/', (req, res) => {
+    res.sendfile(path.join(__dirname + '/index.html'));
+})
 app.get('/ping', (req, res) => {
     res.send('ping')
 });
+app.post('/upload', (req, res) => {
+    let sampleFile;
+    let uploadPath;
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).send('No file were uploded!');
+    }
+    console.log(req.files);
+    sampleFile = req.files.sampleFile;
+
+    uploadPath = __dirname + '/public/' + sampleFile.name;
+
+    sampleFile.mv(uploadPath, function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.send('File uploaded to ' + uploadPath);
+    });
+})
 
 app.listen(port, () => console.log('server is running on port' + port))
